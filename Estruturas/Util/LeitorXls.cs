f2b581b38a13850a135365d76a6fd3cf5d9338e6
Wanderson Ralph Silva Vita <ralph.vita@gmail.com>
2020-11-03 10:03:10 -0300

    Add files via upload

---
+++ Estruturas/Util/LeitorXls.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using Inflor.Infra.Common;
+using Inflor.Infra.Internationalization;
+using log4net;
+
+
+namespace TreinamentoInfra.Util
+{
+    public class LeitorXls
+    {
+        /// <summary>
+        /// Le uma planilha excel e retorna um DataTable com os dados lidos.
+        /// </summary>
+        /// <param name="nomeArquivo">Nome do arquivo excel.</param>
+        /// <param name="pasta">Pasta onde esta alojado o arquivo excel.</param>
+        /// <param name="importMixedTypes">Permite a especificação do IMEX. Se nao informado, considera Text (Recomendável)</param>
+        /// <returns>DataTable com os dados lidos da planilha.</returns>
+        /// 
+        public static DataTable retornaXls(string nomeArquivo, string pasta, string importMixedTypes)
+        {
+            string arquivo = pasta + nomeArquivo;
+            string cnn = string.Empty;
+            string sheetName = string.Empty;
+
+            if (importMixedTypes.ToUpper().Equals("TEXT") || importMixedTypes.ToUpper().Equals("1"))
+            {
+                //Como passar 1 ou TEXT como o valor da propriedade IMEX representa a mesma coisa, estou adotando essa tratativa.
+                cnn = getOfficeProviderConnection(".XLS", arquivo, true, true);
+            }
+            else
+            {
+                cnn = getOfficeProviderConnection(".XLS", arquivo, true, false);
+            }
+
+            try
+            {
+                using (OleDbConnection cn = new OleDbConnection(cnn))
+                {
+                    cn.Open();
+                    sheetName = cn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
+                    string sql = "SELECT * FROM [" + sheetName + "]";
+                    OleDbCommand cmd = new OleDbCommand(sql, cn);
+
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cn.Close();
+
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                //log.Error($@"Erro ao realizar leitura do arquivo Excel
+                //            Parametros: 
+                //                Arquivo: {arquivo}
+                //                Connection String: {cnn}
+                //                Nome da planilha: {sheetName}
+                //            Stack Trace:
+                //                {ex.StackTrace.ToString()}
+                //            ");
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static DataTable retornaXls(string nomeArquivo, string pasta)
+        {
+            return retornaXls(nomeArquivo, pasta, "Text");
+        }
+
+
+        public static DataTable LePlanilhaExcel(string pasta, string nomeArquivo, string extensao, bool TemCabecalho = true, int indexAba = -1)
+        {
+            return LePlanilhaExcel(pasta, nomeArquivo, extensao, null, TemCabecalho, indexAba);
+        }
+
+        /// <summary>
+        /// Lê uma planilha excel e retorna os dados lidos em um DataTable.
+        /// </summary>
+        /// <param name="pasta">Pasta de onde esta situada o arquivo.</param>
+        /// <param name="nomeArquivo">Nome do arquivo.</param>
+        /// <param name="extensao">Extensão do arquivo(XLS ou XLSX).</param>
+        /// <param name="nomeTabela">Nome da Tabela - Nome da tabela tem que ser passado com $ ao final do nome, Exemplo: TESTE$</param>
+        /// <param name="TemCabecalho"></param>
+        /// <param name="indexAba">index da aba a ser considerada na leitura</param>
+        /// <returns>DataTable com os dados lidos da planilha.</returns>
+        public static DataTable LePlanilhaExcel(string pasta,
+                                                string nomeArquivo,
+                                                string extensao,
+                                                string nomeTabela,
+                                                bool TemCabecalho = true,
+                                                int indexAba = -1)
+        {
+            string arquivo = pasta + nomeArquivo;
+            string cnn = string.Empty;
+            DataTable dt = new DataTable();
+            string sheetName = string.Empty;
+            int qtdAbas = indexAba;
+
+            try
+            {
+                cnn = getOfficeProviderConnection(extensao.ToUpper(), arquivo, true, TemCabecalho ? true : false);
+
+
+                if (!string.IsNullOrEmpty(cnn))
+                {
+                    using (OleDbConnection cn = new OleDbConnection(cnn))
+                    {
+                        cn.Open();
+
+                        qtdAbas = cn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows.Count;
+
+                        int indexAbaLeitura = indexAba >= 0 ? indexAba : qtdAbas - 1;
+
+                        if (string.IsNullOrEmpty(nomeTabela))
+                            sheetName = cn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[indexAbaLeitura]["TABLE_NAME"].ToString();
+                        else
+                            sheetName = nomeTabela;
+
+
+                        if (qtdAbas > 0)
+                        {
+                            //tratamento para quando a planilha tiver algum filtro, isso faz com que o execel altere o nome da aba adicionando "_" no final e gerando erro na leitura.
+                            if (sheetName.EndsWith("_"))
+                                sheetName = sheetName.TrimEnd('_');
+                            string sql = "SELECT * FROM [" + sheetName + "]";
+                            OleDbCommand cmd = new OleDbCommand(sql, cn);
+
+                            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                            da.Fill(dt);
+
+                        }
+
+                        cn.Close();
+                    }
+                }
+
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                //log.Error($@"Erro ao realizar leitura do arquivo Excel
+                //            Parametros: 
+                //                Arquivo: {arquivo}
+                //                Connection String: {cnn}
+                //                Nome da planilha: {sheetName}
+                //                Quantidades de abas da Planilha: {qtdAbas}
+                //            Stack Trace:
+                //                {ex.StackTrace.ToString()}
+                //            ");
+                throw new CommonException(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Retorna um array de strings com o nome de cada coluna da planilha excel.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string[] GetNomeColunasPlanilha(DataTable dt)
+        {
+            string[] nomeColunas = new string[dt.Columns.Count];
+
+            if (dt.Columns.Count > 0)
+            {
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    nomeColunas[i] = dt.Columns[i].ColumnName.Replace('#', ' ');
+                }
+            }
+
+            return nomeColunas;
+        }
+
+        public static string FormataVlr(string valor)
+        {
+            CultureInfo cultura = new CultureInfo(CommonConfig.UserLanguage);
+
+            if (cultura.NumberFormat.CurrencyDecimalSeparator.Equals("."))
+            {
+                valor = valor.Replace(',', '.');
+            }
+            else
+            {
+                valor = valor.Replace('.', ',');
+            }
+
+            return valor;
+        }
+
+        /// <summary>
+        /// Retorna a string do valor numerico com separador decimal padrao.
+        /// </summary>
+        /// <param name="strValor"></param>
+        /// <returns>string com valor formatado</returns>
+        public static string FormataVlrPadrao(string strValor)
+        {
+            Double vlrNum;
+            CultureInfo cultura = new CultureInfo(CommonConfig.UserLanguage);
+
+            if (cultura.NumberFormat.CurrencyDecimalSeparator.Equals("."))
+            {
+                strValor = strValor.Replace(",", "");
+                strValor = strValor.Replace(".", ",");
+            }
+            else
+            {
+                strValor = strValor.Replace(".", "");
+            }
+
+            if (!Double.TryParse(strValor, NumberStyles.Float, CultureInfo.CreateSpecificCulture("pt-BR"), out vlrNum))
+                throw new CommonException(Message.Get("#sys.formato.numerico.incorreto"));
+
+            return strValor;
+        }
+
+        /// <summary>
+        /// Cria uma string de conexão para o processo de leitura de arquivos do Excel, inserindo as propriedades necessárias.
+        /// </summary>
+        /// <param name="fileExtension">Extensão do arquivo Excel que será importado, XLS ou XLSX</param>
+        /// <param name="file">Caminho do arquivo que será importado</param>
+        /// <param name="HDR">Indica se deverá considerar HDR, caso seja falso a string de conexão retornada não terá essa propriedade</param>
+        /// <param name="IMEX">Indica de deverá considerar IMEX, caso seja falso a string de conexão retornada não terá essa propriedade</param>
+        /// <returns>String de conexão para ser utilizada em leitura de arquivos Excel</returns>
+        public static string getOfficeProviderConnection(string fileExtension, string file, bool HDR, bool IMEX)
+        {
+            string conn, providerExcel, propExcel, HDRProp, IMEXProp;
+            conn = providerExcel = propExcel = HDRProp = IMEXProp = string.Empty;
+
+
+            if (HDR)
+            {
+                HDRProp = "HDR = Yes;";
+            }
+
+            if (IMEX)
+            {
+                IMEXProp = "IMEX = 1;";
+            }
+
+            switch (fileExtension.ToUpper())
+            {
+                case ".XLS":
+                    providerExcel = SysParameter.GetParameter("PROVIDER.EXCEL", @"Provider=Microsoft.ACE.OLEDB.12.0");
+                    propExcel = SysParameter.GetParameter("PROVIDER.EXCEL.XLS.EXT.PROP", @"Excel 8.0");
+
+                    conn = ($@"{providerExcel}; Data Source={file};Extended Properties='{propExcel};{HDRProp}{IMEXProp}'");
+                    break;
+
+                case ".XLSX":
+                    providerExcel = SysParameter.GetParameter("PROVIDER.EXCEL", @"Provider=Microsoft.ACE.OLEDB.14.0");
+                    propExcel = SysParameter.GetParameter("PROVIDER.EXCEL.XLSX.EXT.PROP", @"Excel 14.0");
+
+                    conn = ($@"{providerExcel}; Data Source={file};Extended Properties='{propExcel};{HDRProp}{IMEXProp}'");
+                    break;
+
+                default:
+                    throw new Exception("sys.label.formato.invalido");
+            }
+
+            return conn;
+        }
+    }
+}
---
+++ Estruturas/Util/SysUtil.cs
@@ -0,0 +1,285 @@
+﻿using Inflor.Infra.Common;
+using Inflor.Infra.Internationalization;
+using Inflor.Infra.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace TreinamentoInfra.Util
+{
+    public class SysUtil
+    {
+        /// <summary>
+        /// Verifica se a extensao de um arquivo é uma planilha excel.
+        /// </summary>
+        /// <param name="extensao">Extensão do arquivo.</param>
+        /// <returns>Retorna true ou false.</returns>
+        public static bool IsPlanilhaExcel(string extensao)
+        {
+            Regex regex = new Regex(@"xls[x]?".ToUpper());
+
+            Match match = regex.Match(extensao.ToUpper());
+
+            return match.Success;
+        }
+
+        public static void UploadArquivo(string path, string nomeArquivo, ref FileUpload upload)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                upload.SaveAs(path + nomeArquivo);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Função que retornara o caminho da pasta onde deverá ser gravado os arquivos de upload
+        /// </summary> 
+        /// <param name="key">Chave cadastrada no banco</param>
+        /// <returns>retorna o caminho da pasta no diretorio</returns>
+        /// <exception cref="Teste">Ex</exception>
+        public static string GetPastaUpload(string key)
+        {
+            return GetPastaUpload(new DataAccessController(), key);
+        }
+
+        /// <summary>
+        /// Função que retornara o caminho da pasta onde deverá ser gravado os arquivos de upload
+        /// </summary> 
+        /// <param name="key">Chave cadastrada no banco</param>
+        /// <returns>retorna o caminho da pasta no diretorio</returns>
+        /// <exception cref="Teste">Ex</exception>
+        public static string GetPastaUpload(DataAccessController dac, string key)
+        {
+            object caminho = dac.ExecuteEscalar(string.Format(@"select NOM_PASTA from SIS_PASTA_UPLOAD 
+                                                        where upper(DCR_ID_PASTA)=upper('{0}') and cd_unidade={1}", key, CommonConfig.UserUnit));
+
+            if (caminho != null)
+            {
+                return CommonConfig.GetKey("PastaBaseUpload") + caminho.ToString();
+            }
+            else
+            {
+                throw new CommonException(Message.TranslateText(string.Format("#sys.alert.chave.nao.cadastrada. #sys.alert.verifique.chave.upload: {0}", key), true)); //CommonException(Message.Get("#sys.alert."));
+            }
+        }
+        public static void SetMsgValorVazio(ref string dcrErro, string nomeColuna)
+        {
+            dcrErro += nomeColuna + ": " + Message.Get("#sys.erro.valor.vazio") + "; ";
+        }
+
+        public static void SetMsgValorInvalido(ref string dcrErro, string nomeColuna)
+        {
+            dcrErro += nomeColuna + ": " + Message.Get("#sys.erro.valor.invalido") + "; ";
+        }
+
+        /// <summary>
+        /// Retorna o tipo do banco (DbType) de acordo com o tipo do objeto passado como parametro
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static DbType GetDbType(object obj)
+        {
+            if (obj == null)
+            {
+                return DbType.String;
+            }
+            else
+            {
+                switch (Type.GetTypeCode(obj.GetType()))
+                {
+                    case TypeCode.DateTime: return DbType.DateTime;
+                    case TypeCode.Decimal: return DbType.Decimal;
+                    case TypeCode.Double:
+                    case TypeCode.Single: return DbType.Double;
+                    case TypeCode.Int16:
+                    case TypeCode.Int32:
+                    //case TypeCode.Int64: return DbType.Int32; Alterado para retornar Int64, pois para variavei do tipo long estava dando erro.
+                    case TypeCode.Int64: return DbType.Int64;
+                    default: return DbType.String;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executa sql efetuando replace dos parametros no formato [nomeParam] para nao dar replace em string e enviar parametros ao Oracle.
+        /// O dicionario com os parametros deve ter a sua key com o mesmo nome que o parametro no sql. Exemplo:
+        ///     key = cdUsuario => parametro no sql = [cdUsuario]
+        /// Parametros repetidos nao precisam ser adicionados duas vezes ao dicionario.
+        /// Parametros nao precisam ser adicionados em ordem ao dicionario.
+        /// </summary>
+        /// <param name="sql">sql a ser executado no banco</param>
+        /// <param name="parametros">dicionario com parametros</param>
+        /// <param name="dac"></param>
+        /// <param name="fill">Caso true indica que retorna um DataTable</param>
+        /// <returns>DataTable ou Object, dependendo do parametro fill</returns>
+        private static object ExecuteQuery(string sql, Dictionary<string, object> parametros, DataAccessController _dac, bool fill = false)
+        {
+            if (string.IsNullOrWhiteSpace(sql)) return null;
+
+            parametros = parametros ?? new Dictionary<string, object>();
+            string prefixo = "[";
+            string sufixo = "]";
+            using (DataAccessController dac = _dac ?? new DataAccessController())
+            {
+                OleDbCommand comando = new OleDbCommand();
+
+                // Expressao regular para encontrar qualquer texto entre [ e ]
+                MatchCollection matches = Regex.Matches(sql, $@"\{prefixo}(.*?)\{sufixo}", RegexOptions.None);
+
+                foreach (Match match in matches)
+                {
+                    string prop = match.Value.Replace(prefixo, string.Empty).Replace(sufixo, string.Empty);
+
+                    if (parametros.Keys.Any(x => x == prop))
+                    {
+                        OleDbParameter paramDb = new OleDbParameter(match.Value, parametros[prop])
+                        {
+                            DbType = GetDbType(parametros[prop])
+                        };
+                        comando.Parameters.Add(paramDb);
+                    }
+                }
+
+                foreach (Match match in matches)
+                {
+                    sql = sql.Replace(match.Value, "?");
+                }
+
+                comando.CommandText = sql;
+
+                if (fill) return dac.Fill(comando);
+                else return dac.ExecuteEscalar(comando);
+            }
+        }
+
+        /// <summary>
+        /// Executa sql efetuando replace dos parametros no formato ? para nao dar replace em string e enviar parametros ao Oracle.
+        /// Os parametros devem ser passados na mesma ordem que eles aparecem na consulta SQL
+        /// Parametros repetidos devem ser passados mais de uma vez.
+        /// Exemplo:
+        ///     sql: SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = ? AND DCR_LOGIN LIKE ?
+        ///     parametros: cdUsuario = 1, login = "inflor%"
+        /// </summary>
+        /// <param name="sql">sql a ser executado no banco</param>
+        /// <param name="dac"></param>
+        /// <param name="parametros">parametros em ordem que aparecem no select</param>
+        private static object ExecuteQuery(string sql, DataAccessController _dac, bool fill, params object[] parametros)
+        {
+            if (string.IsNullOrWhiteSpace(sql)) return null;
+
+            using (DataAccessController dac = _dac ?? new DataAccessController())
+            {
+                OleDbCommand comando = new OleDbCommand();
+                if (parametros?.Length > 0)
+                {
+                    foreach (object param in parametros)
+                    {
+                        OleDbParameter dbp = new OleDbParameter()
+                        {
+                            Value = param,
+                            DbType = GetDbType(param)
+                        };
+                        comando.Parameters.Add(dbp);
+                    }
+                }
+
+                comando.CommandText = sql;
+                if (fill) return dac.Fill(comando);
+                else return dac.ExecuteEscalar(comando);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Executa sql efetuando replace dos parametros no formato ? para nao dar replace em string e enviar parametros ao Oracle.
+        /// Os parametros devem ser passados na mesma ordem que eles aparecem na consulta SQL
+        /// Parametros repetidos devem ser passados mais de uma vez.
+        /// Exemplo:
+        ///     sql: SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = ? AND DCR_LOGIN LIKE ?
+        ///     parametros: cdUsuario = 1, login = "inflor%"
+        /// </summary>
+        /// <param name="sql">sql a ser executado no banco</param>
+        /// <param name="dac"></param>
+        /// <param name="parametros">parametros em ordem que aparecem no select</param>
+        public static DataTable ExecuteFill(string sql, DataAccessController dac, params object[] parametros)
+        {
+            return (DataTable)ExecuteQuery(sql, dac, true, parametros);
+        }
+
+        /// <summary>
+        /// Executa sql efetuando replace dos parametros no formato [nomeParam] para nao dar replace em string e enviar parametros ao Oracle.
+        /// O dicionario com os parametros deve ter a sua key com o mesmo nome que o parametro no sql. Exemplo:
+        ///     key = cdUsuario => parametro no sql = [cdUsuario]
+        /// Parametros repetidos nao precisam ser adicionados duas vezes ao dicionario.
+        /// Parametros nao precisam ser adicionados em ordem ao dicionario.
+        /// No caso de comparacao por LIKE utilizar a porcentagem dentro da string. LIKE "inflor%"
+        ///     sql: SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = [cdUsuario] AND DCR_LOGIN LIKE [dcrLogin]
+        /// </summary>
+        /// <param name="sql">sql a ser executado no banco</param>
+        /// <param name="dac"></param>
+        /// <param name="parametros">dicionario com parametros</param>
+        /// <returns></returns>
+        public static object ExecuteSelect(string sql, DataAccessController dac, Dictionary<string, object> parametros)
+        {
+            /*
+             * Exemplo:
+            int cdUsuario = 1;
+            string dcrLogin = "%inflor%";
+            
+            string sqlExemplo = $"SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = [{nameof(cdUsuario)}] AND DCR_LOGIN LIKE [{nameof(dcrLogin)}] AND CD_USUARIO = [{nameof(cdUsuario)}]";
+
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add(nameof(dcrLogin), dcrLogin);
+            dic.Add(nameof(cdUsuario), cdUsuario);
+            
+
+            ExecuteSelect(sqlExemplo, dac, dic);
+            */
+
+            return ExecuteQuery(sql, parametros, dac);
+        }
+
+        /// <summary>
+        /// Executa sql efetuando replace dos parametros no formato ? para nao dar replace em string e enviar parametros ao Oracle.
+        /// Os parametros devem ser passados na mesma ordem que eles aparecem na consulta SQL
+        /// Parametros repetidos devem ser passados mais de uma vez.
+        /// Exemplo:
+        ///     sql: SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = ? AND DCR_LOGIN LIKE ?
+        ///     parametros: cdUsuario = 1, login = "inflor%"
+        /// </summary>
+        /// <param name="sql">sql a ser executado no banco</param>
+        /// <param name="dac"></param>
+        /// <param name="parametros">parametros em ordem que aparecem no select</param>
+        /// <returns></returns>
+        public static object ExecuteSelect(string sql, DataAccessController dac, params object[] parametros)
+        {
+            /*
+            int cdUsuario   = 1;
+            string dcrLogin = "%inflor%";
+            
+            string sqlExemplo = $"SELECT DCR_LOGIN FROM SEG_USUARIO WHERE CD_USUARIO = ? AND DCR_LOGIN LIKE ? AND CD_USUARIO = ?";
+            
+            ExecuteSelect(sqlExemplo, dac, cdUsuario, dcrLogin, cdUsuario);
+            */
+
+
+            return ExecuteQuery(sql, dac, false, parametros);
+        }
+    }
+}
